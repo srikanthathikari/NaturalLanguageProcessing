e25843b6dc8d0e338b5bfdd16e246f2c1c080678
Srikanth <srikantathikari@gmail.com>
2021-06-16 15:27:57 -0400

    Tests Added with Jest

---
+++ test/formHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../src/client/js/formHandler";
+// The describe() function
+describe("Testing the submit functionality", () => {
+    // The test() function
+    test("Testing the handleSubmit() function", () => {
+           expect(handleSubmit).toBeDefined();
+})});
---
+++ test/nameChecker.test.js
@@ -0,0 +1,7 @@
+import { checkForName } from "../src/client/js/nameChecker";
+// The describe() function
+describe("Testing the URL validity", () => {
+    // The test() function
+    test("Testing the checkForName() function", () => {
+           expect(checkForName).toBeDefined();
+})});
